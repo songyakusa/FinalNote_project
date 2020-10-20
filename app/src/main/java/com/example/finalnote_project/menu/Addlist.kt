d0ed25ce2f184952246e235da2c2fd003344ec3c
CHARAN <songyakusa@gmail.com>
2020-10-20 16:19:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/finalnote_project/menu/Addlist.kt
@@ -0,0 +1,42 @@
+package com.example.finalnote_project.menu
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.finalnote_project.R
+import com.example.finalnote_project.databinding.FragmentAddlistBinding
+
+
+class Addlist : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var binding = DataBindingUtil.inflate<FragmentAddlistBinding>(
+            inflater,
+            R.layout.fragment_addlist,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
+
+}
